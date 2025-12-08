12c5c2a0c73b2377491b01dcfa35769f95adddc0
damiannelus <damiannelus@gmail.com>
2025-12-08 21:23:37 +0100

    Update briefings for <data>

---
+++ sync_command.ps1
@@ -0,0 +1,9 @@
+   cd C:\Programowanie\vibe\briefings-public
+
+   python sync_surfer.py        # skopiuj nowe md z news_feed_analyzer
+   python generate_indexes.py   # zaktualizuj index.json
+
+   git status
+   git add .
+   git commit -m "Update briefings for <data>"
+   git push
